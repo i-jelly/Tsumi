c10782a683fbb06f93b0b6d4fcc23fa36376feac
lan760658265@foxmail.com <lan760658265@foxmail.com>
2020-08-06 20:06:51 +0800

    Setup Bot main thread

---
+++ Controller/群消息事件.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Mirai_CSharp;
+using Mirai_CSharp.Models;
+using Mirai_CSharp.Plugin.Interfaces;
+
+namespace Tsuki.Controller
+{
+    public partial class 群消息事件 : IGroupMessage
+    {
+        public async Task<bool> GroupMessage(MiraiHttpSession session, IGroupMessageEventArgs e)
+        {
+            IMessageBase[] chain = new IMessageBase[]
+            {
+                new PlainMessage($"{e.Sender.Name}:{string.Join(null, (IEnumerable<IMessageBase>)e.Chain)}")
+            };
+            await session.SendGroupMessageAsync(e.Sender.Group.Id, chain);
+            return true;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Mirai_CSharp;
+using Mirai_CSharp.Models;
+using Tsuki.Controller;
 
 namespace Tsuki
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start\n");
+            MiraiHttpSessionOptions options = new MiraiHttpSessionOptions("127.0.0.1", 23333, "1145141919810");
+            await using MiraiHttpSession session = new MiraiHttpSession();
+
+            群消息事件 plugin = new 群消息事件();
+            session.AddPlugin(plugin);
+
+            await session.ConnectAsync(options, 3178223002);
+            while (true)
+            {
+                if(await Console.In.ReadLineAsync() == "exit")
+                {
+                    return;
+                }
+            }
         }
     }
 }
